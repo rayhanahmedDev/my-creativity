d4ebec5562bfc75d1018736ca2216f4fc3018dcb
rayhan9802 <mdrayhaanahmed@gmail.com>
2024-01-11 18:46:13 +0600

    install packages what i need

---
+++ src/Pages/Home/Home/Home.jsx
@@ -0,0 +1,14 @@
+import Navber from "../Navber/Navber";
+
+
+const Home = () => {
+    return (
+        <div>
+            <div>
+                <Navber></Navber>
+            </div>
+        </div>
+    );
+};
+
+export default Home;
